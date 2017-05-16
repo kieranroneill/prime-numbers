d703d1caad8807abfe746a00b44b9e03bad8c904
Kieran O'Neill <kieranroneill@googlemail.com>
2017-05-16 13:10:45 +0100

    Adds enzyme for react testing

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
 import Promise from 'bluebird';
 import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
 import { JSDOM } from 'jsdom';
 import { assert, mock, spy, stub } from 'sinon';
 
@@ -18,3 +19,5 @@ global.app = app;
 
 // Client globals.
 global.document = new JSDOM('<!doctype html><html><body></body></html>');
+global.mount = mount;
+global.shallow = shallow;
